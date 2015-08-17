22017df21a13d91b466163d21e1cd1d5765fe33c
lucasgmartins <lucas.gmmartins@gmail.com>
2015-08-16 21:04:18 -0300

    fix all tests

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-cd flice-projects/flice-account/server && mvn clean package -DskipTests && cd ../../../;
-cd flice-projects/flice-core/server    && mvn clean package -DskipTests && cd ../../../;
+cd projects/account/server && mvn clean package -DskipTests && cd ../../../;
+cd projects/core/server    && mvn clean package -DskipTests && cd ../../../;
--- projects/account/server/src/main/java/com/timeoutzero/flice/account/repository/ProductRepository.java
+++ projects/account/server/src/main/java/com/timeoutzero/flice/account/repository/ProductRepository.java
@@ -1,15 +1,10 @@
 package com.timeoutzero.flice.account.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import com.timeoutzero.flice.account.entity.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-	@Query("SELECT WHEN count(p) > 0 true ELSE false END FROM Product p WHERE p.token = :token")
-	boolean existByToken(@Param("token") String token);
-	
 	Product findByToken(String token);
 }
