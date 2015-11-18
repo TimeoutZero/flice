9e91272fc9e0d85b229f9b97edcfbc2eaea47b17
lucasgmartins <lucas.gmmartins@gmail.com>
2015-11-18 13:34:35 -0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-mvn clean package  -f projects/account/server/pom.xml
-mvn clean package  -f projects/core/server/pom.xml
+mvn clean package -DskipTests -f projects/account/server/pom.xml
+mvn clean package -DskipTests -f projects/core/server/pom.xml
