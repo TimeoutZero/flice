bb4b480f0d6bd545830d2d9903f484ba444c3920
lucasgmartins <lucas.gmmartins@gmail.com>
2016-03-17 23:00:25 -0300

    Add build server

--- build/scripts/build-server.sh
+++ build/scripts/build-server.sh
@@ -1,6 +1,4 @@
 
-cd ../../
-
 mvn clean install -DskipTests -f projects/account/client/pom.xml
 mvn test -DskipTests -f projects/account/server/pom.xml
 mvn test -DskipTests -f projects/core/server/pom.xml
